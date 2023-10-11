58c816ffc66b6b8664359d6f7f1cc4d0e60f553c
Ricky Carter <randolphcarter5@gmail.com>
2023-10-11 14:04:48 -0400

    updated connection to MONGDB_URI w/ use of best practices

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Connect to Mongoose; Best practice, hide MongoDB URI in .env for security purposes;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/Social-Network')
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => console.log('Mongoose connected'))
     .catch(err => console.log(err));
 

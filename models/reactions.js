ae78e7019829dbf24988165118c60df3150164ca
Ricky Carter <randolphcarter5@gmail.com>
2023-10-11 12:04:12 -0400

    created reaction schema for document construction in collection.

---
+++ models/reactions.js
@@ -0,0 +1,38 @@
+const mongoose = require("mongoose");
+const dayjs = require("dayjs");
+
+// Schema to create Reaction model
+const reactionSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    reactionId: {
+    // assigning new objectID by default 
+      type: mongoose.Schema.Types.ObjectId,
+      default: () => new mongoose.Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    // Add getter method to format timestamp on query
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (timeStamp) => {
+        return dayjs(timeStamp).format("MMM D YYYY [at] h:mm A");
+      },
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
